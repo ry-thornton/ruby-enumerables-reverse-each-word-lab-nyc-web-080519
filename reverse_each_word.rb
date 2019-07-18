c573f1b1d0bf0a762ddf8c1b545824927f8da332
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-18 17:02:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(sentence)
   new_array = []
   new_array = sentence.split(" ")
   result = []
-  new_array.each do |new_array|
-    result.push(new_array.reverse)
+  new_array.collect do |new_array|
+    new_array.reverse
   end
-  return result.join(" ")
+  new_array.join(" ")
 end
 
