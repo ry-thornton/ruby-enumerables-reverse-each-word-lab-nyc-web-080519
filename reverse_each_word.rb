9b43182557faea791bddb7a86cd45045794c7607
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-18 16:55:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(sentence)
   new_array = []
   new_array = sentence.split(" ")
+  result = []
   new_array.each do |new_array|
+    result.push(new_array.reverse)
+  end
+  return result
+end
+
